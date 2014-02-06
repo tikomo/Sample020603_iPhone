8c01b99e4c2a2238c1a1cb82be19642a3c9a3cd9
tikomo <tikomo@Komorita-no-MacBook-Air.local>
2014-02-06 17:04:39 +0900

    Initial Commit

---
+++ Sample020603/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sample020603
+//
+//  Created by tikomo on 2014/02/06.
+//  Copyright (c) 2014年 tikomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sample020603/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sample020603
+//
+//  Created by tikomo on 2014/02/06.
+//  Copyright (c) 2014年 tikomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
